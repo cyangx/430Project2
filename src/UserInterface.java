47b5a2c59b0f104f4b89315d5c928aa6be3dd2b2
Eric Dorphy <edorphy@gmail.com>
2014-03-20 17:02:32 -0500

    Revert "Modified SalesState, also added IOHelper"
    This reverts commit a370b9a44fd8deff533eb8573cf4e1028a1e2a10.

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -267,6 +267,7 @@ public class UserInterface {
     }
 
     public void acceptShipment() {
+
         do {
             String productId = getToken("Enter product ID: ");
             Product p = warehouse.findProduct(productId);
@@ -412,8 +413,10 @@ public class UserInterface {
                 Wait waitList = (Wait) (wholeWaitList.next());
                 System.out.println(waitList.toString());
             }
+
         } else {
             System.out.println("Product not found.");
+
         }
     }
 
